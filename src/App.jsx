7fcd8e9717fd68079950a6f7fafe092cceb8cb33
Marinela Rajkovic <marinela.rajkovic29@gmail.com>
2025-10-28 18:23:24 +0100

    Add TweetForm component and integrate tweet submission functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import TweetForm from "./components/TweetForm";
 
 
  function App() {
+
+  const[tweets, setTweets] =useState([]);
+
+  const handleNewTweet = (tweet) => {
+    setTweets([tweet, ...tweets]);
+  };
+
+
   return (
     <div className="min-h-screen bg-gray-200 text-blue-300">
       <Header />
 
       <main className="pt-24 flex flex-col items-center px-4">
+          <TweetForm onTweet={handleNewTweet} />
+
         <div className="w-full max-w-2xl bg-white shadow-md rounded-xl p-6">
           <h2 className="text-2xl font-semibold text-blue-600 mb-4">
             Home Feed
           </h2>
 
-          <p className="text-gray-600">
-            Here will be tweets!
-          </p>
+          {tweets.length === 0 ? (
+            <p className="text-gray-500">
+              No tweets yet!
+            </p>
+          ) : (
+            <ul className="space-y-4">
+              {tweets.map((tweet) => (
+                <li
+                  key={tweet.id}
+                  className="border-b border-gray-200 pb-3 last:border-none"
+                >
+                  <p className="font-semibold text-blue-600">
+                    {tweet.username}
+                  </p>
+
+                  <p>
+                    {tweet.content}
+                  </p>
+
+                  <span className=" text-sm text-gray-400">
+                    {tweet.createAt}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </main>
     </div>
---
+++ src/components/TweetForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function TweetForm ({onTweet}) {
+    const [content, setContent] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!content.trim()) return;
+
+        const newTweet = {
+            id: Date.now(),
+            userName: "Marin",
+            content,
+            createAt: new Date().toLocaleDateString(),
+        };
+
+        onTweet(newTweet);
+        setContent("");
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded-xl p-4 mb-6 w-full max-w-md"
+        > 
+            <textarea 
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                placeholder="What is happening?"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-400 resize-none"
+                rows="3"
+            />
+
+            <button
+                type="submit"
+                className=" mt-3 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
+            >
+                Tweet
+            </button>
+        </form>
+    );
+}
+
+export default TweetForm;
